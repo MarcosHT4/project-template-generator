3b8b852c5378f1481ed2f3561c3541d213015451
Marcos <marcosht503@gmail.com>
2023-11-29 09:12:31 -0400

    Ultra test

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@ from enum import Enum
 from functools import lru_cache
 from pydantic_settings import BaseSettings
 
-
 class GPTModel(str, Enum):
     gpt_4 = "gpt-4"
     gpt_3_5_turbo = "gpt-3.5-turbo"
@@ -14,6 +13,7 @@ class Settings(BaseSettings):
     log_level: str = "DEBUG"
     openai_key: str
     model: GPTModel = GPTModel.gpt_3_5_turbo
+    yolo_version: str = "yolov8n.pt"
 
     class Config:
         env_file = ".env"
--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI, Depends
+from fastapi import (
+    FastAPI, 
+    UploadFile, 
+    File, 
+    HTTPException, 
+    status,
+    Depends
+
+)
 from starlette.middleware.cors import CORSMiddleware
 from src.llm_service import TemplateLLM
 from src.prompts import ProjectParams
 from src.parsers import ProjectIdeas
 from src.config import get_settings
+import io
+from PIL import Image
+from src.detector import ObjectDetector, Detection
+import numpy as np 
+from functools import cache
 
 _SETTINGS = get_settings()
 
@@ -23,6 +36,33 @@ app.add_middleware(
 
 def get_llm_service():
     return TemplateLLM()
+@cache
+def get_object_detector():
+    print("creating model...")
+    return ObjectDetector()
+
+def predict_uploadfile(predictor, file, threshold):
+    img_stream = io.BytesIO(file.file.read())
+    if file.content_type.split("/")[0] != "image":
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, 
+            detail="Not an image"
+        )
+    # convertir a una imagen de Pillow
+    img_obj = Image.open(img_stream)
+    # crear array de numpy
+    img_array = np.array(img_obj)
+    return predictor.predict_image(img_array, threshold), img_array
+
+@app.post("/objects")
+def detect_objects(
+    threshold: float = 0.5,
+    file: UploadFile = File(...), 
+    predictor: ObjectDetector = Depends(get_object_detector)
+):
+    results, _ = predict_uploadfile(predictor, file, threshold)
+    
+    return results
 
 
 @app.post("/generate")
